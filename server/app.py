2fc4bdab81cfeb306962fd02a5885ad1fb95a3d6
Kitui Kelly <kituikelly@gmail.com>
2024-06-26 01:35:54 +0300

    completes lab

--- server/app.py
+++ server/app.py
@@ -21,6 +21,32 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.get(id)
+
+    if earthquake is not None:
+        body = earthquake.to_dict()
+        body['year'] = int(body['year'])  # Convert year from string to int
+        return make_response(body, 200)
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        return make_response(body, 404)
+
+@app.route('/earthquakes/magnitude/<float:Emagnitude>')
+def earthquake(Emagnitude):
+    
+    earthquakes =[]
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= Emagnitude).all():
+        earthquake = earthquake.to_dict()
+        earthquake['year'] = int(earthquake['year'])  # Convert year from string to int
+        earthquakes.append(earthquake)      
+    body = {'count': len(earthquakes),
+            'quakes': earthquakes
+            }
+    return make_response(body, 200)
+
+
 
 
 if __name__ == '__main__':
